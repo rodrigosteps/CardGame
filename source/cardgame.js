59879f32fd01eaf6affaf6d2543b8d27c7eb3b8f
RODRIGO PASSOS <rodrigo.ctto@gmail.com>
2026-06-02 22:44:23 -0300

    update

--- source/cardgame.js
+++ source/cardgame.js
@@ -1,9 +1,9 @@
 
 const imagens = [
-    'sukunaCard.webp', 'itadoriCard.webp', 'nobaraCard.webp', 
-    'gojoCard.webp', 'pandaCard.webp', 'jogoCard.webp', 
-    'todoCard.webp', 'nanamiCard.webp', 'hanamiCard.webp', 
-    'inumakiCard.webp', 'makiCard.webp', 'mahitoCard.webp', 
+    'sukunaCard.webp', 'itadoriCard.webp', 'nobaraCard.webp',
+    'gojoCard.webp', 'pandaCard.webp', 'jogoCard.webp',
+    'mahogaraCard.webp', 'nanamiCard.webp', 'hanamiCard.webp',
+    'inumakiCard.webp', 'makiCard.webp', 'mahitoCard.webp',
     'megumiCard.webp'
 ];
 
@@ -15,60 +15,53 @@ const precarregarImagens = () => {
     });
 };
 
-// Executa o preload quando a janela carregar
-window.onload = precarregarImagens;
-
+window.onload = precarregarImagens; // Executa o preload quando a janela carregar
 
 //funcção para escolher uma carta
 
 function pickup() {
-
-const audio = document.getElementById('audioPlay');
-  setTimeout(()=>{audio.play();},400) 
+//carregar som de carta ao clickar no botão
+    const audio = document.getElementById('audioPlay');
+    setTimeout(() => { audio.play(); }, 400)
+//constantes da func.
     const playerName = document.getElementById("nome").value;
     const playerDate = document.getElementById("date").value;
     const imgTag = document.getElementById("Personagem");
     const playerDisplay = document.getElementById("playerid");
+//caminho para carregar as imagens
     const pasta = './source/assets/'
-            const imagens = [
-            'sukunaCard.webp',
-            'itadoriCard.webp',
-            'nobaraCard.webp',
-            'gojoCard.webp',
-            'pandaCard.webp',
-            'jogoCard.webp',
-            'todoCard.webp',
-            'nanamiCard.webp',
-            'hanamiCard.webp',
-            'inumakiCard.webp',
-            'makiCard.webp',
-            'mahitoCard.webp',
-            'megumiCard.webp'];
-            
-            console.log(pasta)
-            
-            
-    //validação de infos
+    const imagens = [
+        'sukunaCard.webp',
+        'itadoriCard.webp',
+        'nobaraCard.webp',
+        'gojoCard.webp',
+        'pandaCard.webp',
+        'jogoCard.webp',
+        'mahogaraCard.webp',
+        'nanamiCard.webp',
+        'hanamiCard.webp',
+        'inumakiCard.webp',
+        'makiCard.webp',
+        'mahitoCard.webp',
+        'megumiCard.webp'];
 
+//validação de infos
     if (playerName.trim() !== "" && playerDate !== "") {
         playerDisplay.innerHTML = `${playerName}, No mundo de Jujutsu você seria:`;
+//gatilho da func.
+        const indice = Math.floor(Math.random() * imagens.length);
 
-            const indice = Math.floor(Math.random() * imagens.length);
+        const escolhida = imagens[indice];
 
-            const escolhida = imagens[indice];
+        imgTag.src = `${pasta}${escolhida}`;
+        imgTag.classList.remove('resultado');
 
-            imgTag.src= `${pasta}${escolhida}`;
-            imgTag.classList.remove('resultado');
+        void imgTag.offsetWidth; //Força o navegador a "renderizar" o elemento sem a classe
+       
+        imgTag.classList.add('resultado');  //Adiciona a classe novamente para disparar a animação
+    }
 
-        // 2. TRUQUE: Força o navegador a "renderizar" o elemento sem a classe
-        void imgTag.offsetWidth; 
-
-
-        // 3. Adiciona a classe novamente para disparar a animação
-        imgTag.classList.add('resultado');
-        }
-        
     else {
-        playerDisplay.innerHTML ='Preencha seu nome e sua idade';
+        playerDisplay.innerHTML = 'Preencha seu nome e sua idade';//fallback das infos
     }
 }
